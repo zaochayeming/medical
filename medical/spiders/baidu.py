97284e032ad7a4fb7c549403876305f66a0a7506
zaochayeming <33992941+zaochayeming@users.noreply.github.com>
2022-04-07 21:58:32 +0800

    Initial commit

---
+++ medical/spiders/baidu.py
@@ -0,0 +1,11 @@
+import scrapy
+
+
+class BaiduSpider(scrapy.Spider):
+    name = 'baidu'
+    allowed_domains = ['baidu.com']
+    start_urls = ['http://baike.baidu.com/item/急性阑尾炎']
+
+    def parse(self, response):
+        print(response.status)
+        pass
